933ad9380c6305329d663018e0bcdbbbb3abb261
Thuong <hoaithuong3454@gmail.com>
2020-12-27 19:24:20 +0700

    QLCH hoan thien chuc nang quan ly nguyen lieu

--- TestDoAn/Controllers/QLCHController.cs
+++ TestDoAn/Controllers/QLCHController.cs
@@ -10,5 +10,139 @@ namespace TestDoAn.Controllers
 {
     public class QLCHController : Controller
     {
+private Models.QuanLyCuaHangEntites db = new Models.QuanLyCuaHangEntites();
+        // GET: Qlch
+        
+		public ActionResult IndexNL()
+		{
+
+			return View(db.NguyenLieux.ToList());
+		}
+		public ActionResult FormThemNL()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult themNL(Models.NguyenLieu a)
+		{
+			if (ModelState.IsValid)
+			{
+				NguyenLieu b = db.NguyenLieux.Find(a.manl);
+				if(b==null)
+                {
+					db.NguyenLieux.Add(a);
+					db.SaveChanges();
+					return RedirectToAction("IndexNL");
+				}
+				ModelState.AddModelError("manl", "Mã Nguyên Liệu đã được sử dụng");
+			}
+			return View("FormThemNL");
+		}
+		public ActionResult FormXoaNL(string id)
+		{
+			bool Xoa = true;
+			Models.NguyenLieu a = db.NguyenLieux.Find(id);
+			foreach (Models.ChiTietSanPham t in db.ChiTietSanPhams.Where(x => x.manl == id).ToList())
+			{					
+				Xoa = false;
+				break;
+			}
+			ViewBag.XoaNguyenLieu = Xoa;
+			
+			if (a != null)
+			{
+				return View(a);
+			}
+			return RedirectToAction("IndexNL");
+		}
+
+		public ActionResult xoaNL(string id)
+		{
+			Models.NguyenLieu a = db.NguyenLieux.Find(id);
+			if (a != null)
+			{
+					db.NguyenLieux.Remove(a);
+					db.SaveChanges();
+			}
+			return RedirectToAction("IndexNL");
+
+		}
+		public ActionResult FormSuaNL(string id)
+		{
+			Models.NguyenLieu a = db.NguyenLieux.Find(id);
+			if (a != null)
+				return View(a);
+			return RedirectToAction("IndexNL");
+		}
+		[HttpPost]
+		public ActionResult suaNL(Models.NguyenLieu b)
+		{
+
+			Models.NguyenLieu a = db.NguyenLieux.Find(b.manl);
+			if (a != null)
+			{
+				a.tenhh = b.tenhh;
+				a.soluong = b.soluong;
+				a.maloai = b.maloai;
+				a.hsd = b.hsd;
+				a.nhacungcap = b.nhacungcap;
+				a.dvt = b.dvt;
+				a.dongia = b.dongia;
+				db.SaveChanges();
+				return RedirectToAction("IndexNL");
+			}
+			return View("FormSuaNL");
+		}
+		[HttpGet]
+		public ActionResult xoahoadon(int id)
+        {
+			HoaDon a = db.HoaDons.Find(id);
+			List<ChiTietHoaDon> ds = db.ChiTietHoaDons.Where(x => x.mahd == a.mahd).ToList();
+			foreach(var b in ds)
+            {
+                if (b != null)
+                {
+					db.ChiTietHoaDons.Remove(b);
+					db.SaveChanges();
+				}
+			}
+			
+				db.HoaDons.Remove(a);
+				db.SaveChanges();
+            
+			return RedirectToAction("IndexHD");
+        }
+
+		public ActionResult IndexNH()
+        {
+			return View(db.NguyenLieux.ToList());
+        }
+
+		public ActionResult add(string id)
+        {
+			NguyenLieu a = db.NguyenLieux.Find(id);
+			return View(a);
+        }
+
+		[HttpGet]
+		public ActionResult addNL(string id)
+        {
+			NguyenLieu nl = db.NguyenLieux.Find(id);
+			if(nl!=null)
+            {
+				int a = int.Parse(Request["slnhap"].ToString());
+				if (a > 0)
+				{
+					nl.soluong = nl.soluong + a;
+					db.NguyenLieux.AddOrUpdate(nl);
+					db.SaveChanges();
+					return RedirectToAction("IndexNH");
+				}
+				ModelState.AddModelError("slnhap", "Số lượng lớn hơn 0!!!");
+			}
+			return View("add");
+        }
+
+
         }
 }
